3eb5e1b06eed7f1421bf97e61333fcf0ededce67
Jackson Oppenheim <50721527+jacksonopp@users.noreply.github.com>
2022-11-27 10:13:26 -0600

    better screenshot

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,3 @@
-use std::fmt::format;
-
 use crate::models::Model;
 use nannou::prelude::*;
 
@@ -8,8 +6,9 @@ pub fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
 pub fn screenshot(app: &App, seed: usize) {
-    let ext = format!("-seed-{}", seed);
+    let app_name = &app.exe_name().unwrap_or("file".to_owned());
+    let ext = format!("images/{}-{}.png", app_name, seed);
 
     app.main_window()
-        .capture_frame("images/".to_string() + &app.exe_name().unwrap_or("file-".to_owned()) + ext.as_str() + ".png");
+        .capture_frame(ext);
 }
